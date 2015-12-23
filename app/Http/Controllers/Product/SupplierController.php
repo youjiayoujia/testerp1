85a3705d47b1566d123c80736f205aa923324996
mcss <178069409@qq.com>
2015-12-23 14:39:48 +0800

    no message

---
+++ app/Http/Controllers/Product/SupplierController.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ *  供货商控制器
+ *  处理与供货商相关的操作
+ *
+ *  @author:MC<178069409@qq.com>
+ *	Date:2015/12/18
+ *	Time:11:18
+ *
+ */
+
+namespace App\Http\Controllers\product;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\Product\SupplierRepository;
+
+class SupplierController extends Controller
+{
+	protected $supplier;
+
+	public function __construct(Request $request, SupplierRepository $supplier)
+	{
+		$this->supplier = $supplier;
+		$this->request = $request;
+	} 
+
+	/**
+	 * 供货商列表展示
+	 *
+	 * @param none
+	 * @return view 
+	 *
+	 */
+	public function index()
+	{
+		$this->request->flash();
+		$response = [
+			'data' => $this->supplier->auto()->paginate(),
+		];
+
+		return view('Product.Supplier.index', $response);
+	}
+
+	/**
+	 * 显示供货商详细信息
+	 *
+	 * @param $id integer 列表的id
+	 * @return view
+	 *
+	 */
+	public function show($id)
+	{
+		$response = [
+			'supplier' => $this->supplier->get($id),
+		];
+
+		return view('Product.Supplier.show', $response);
+	}
+
+	/**
+	 * 新建一个供货商记录
+	 *
+	 * @param none
+	 * @return view
+	 *
+	 */
+	public function create()
+	{
+		return view('Product.Supplier.create');
+	}
+
+	/**
+	 * 供货商信息保存
+	 *
+	 * @param none
+	 * @return view
+	 *
+	 */
+	public function store()
+	{
+		$this->request->flash();
+		$this->validate($this->request, $this->supplier->rules('create'));
+		$this->supplier->create($this->request->all());
+
+		return redirect(route('Supplier.index'));
+	}
+
+	/**
+	 * 跳转供货商信息编辑页面
+	 *
+	 * @param $id integer 记录id
+	 * @return view
+	 *
+	 */
+	public function edit($id)
+	{
+		$response = [
+			'supplier' => $this->supplier->get($id),
+		];
+
+		return view('Product.Supplier.edit',$response);
+	}
+
+	/**
+	 * 供货商信息更新
+	 *
+	 * @param $id integer 记录id
+	 * @return view
+	 *
+	 */
+	public function update($id)
+	{
+		$this->request->flash();
+		$this->validate($this->request, $this->supplier->rules('update', $id));
+		$this->supplier->update($id, $this->request->all());
+		
+		return redirect(route('Supplier.index'));
+	}
+
+	/**
+	 * 删除记录
+	 *
+	 * @param $id integer 记录id
+	 * @return view	
+	 *
+	 */
+	public function destroy($id)
+	{
+		$this->supplier->destroy($id);
+
+		return redirect(route('Supplier.index'));
+	}
+}

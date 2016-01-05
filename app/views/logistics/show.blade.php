8d115a7971508a2341edcdda5f7a60da0e322380
卞海伟 <bianhaiwei@bianhaiweideMacBook-Pro.local>
2016-01-05 14:41:32 +0800

    no message

--- app/views/logistics/show.blade.php
+++ app/views/logistics/show.blade.php
@@ -1,34 +1,49 @@
-@extends('layouts.default')
-@section('content')
+@extends('common.detail')
+@section('detailBody')
     <div class="panel panel-default">
-        <div class="panel-heading">物流方式详情 : {{ $logistics->short_code }} {{ $logistics->logistics_type }}</div>
+        <div class="panel-heading">基础信息</div>
         <div class="panel-body">
-            <dl class="dl-horizontal">
-                <dt>ID</dt>
-                <dd>{{ $logistics->id }}</dd>
-                <dt>物流方式简码</dt>
-                <dd>{{ $logistics->short_code }}</dd>
-                <dt>物流方式名称</dt>
-                <dd>{{ $logistics->logistics_type }}</dd>
-                <dt>种类</dt>
-                <dd>{{ $logistics->species == 'express' ? '快递' : '小包' }}</dd>
-                <dt>仓库</dt>
-                <dd>{{ $logistics->warehouse->name }}</dd>
-                <dt>物流商</dt>
-                <dd>{{ $logistics->supplier->name }}</dd>
-                <dt>物流商物流方式</dt>
-                <dd>{{ $logistics->type }}</dd>
-                <dt>物流追踪网址</dt>
-                <dd>{{ $logistics->url }}</dd>
-                <dt>API对接方式</dt>
-                <dd>{{ $logistics->api_docking }}</dd>
-                <dt>是否启用</dt>
-                <dd>{{ $logistics->is_enable == 'Y' ? '是' : '否' }}</dd>
-                <dt>创建时间</dt>
-                <dd>{{ $logistics->created_at }}</dd>
-                <dt>更新时间</dt>
-                <dd>{{ $logistics->updated_at }}</dd>
-            </dl>
+            <div class="col-lg-4">
+                <strong>ID</strong>: {{ $logistics->id }}
+            </div>
+            <div class="col-lg-4">
+                <strong>物流方式简码</strong>: {{ $logistics->short_code }}
+            </div>
+            <div class="col-lg-4">
+                <strong>物流方式名称</strong>: {{ $logistics->logistics_type }}
+            </div>
+            <div class="col-lg-4">
+                <strong>种类</strong>: {{ $logistics->species == 'express' ? '快递' : '小包' }}
+            </div>
+            <div class="col-lg-4">
+                <strong>仓库</strong>: {{ $logistics->warehouse->name }}
+            </div>
+            <div class="col-lg-4">
+                <strong>物流商</strong>: {{ $logistics->supplier->name }}
+            </div>
+            <div class="col-lg-4">
+                <strong>物流商物流方式</strong>: {{ $logistics->type }}
+            </div>
+            <div class="col-lg-4">
+                <strong>物流追踪网址</strong>: {{ $logistics->url }}
+            </div>
+            <div class="col-lg-4">
+                <strong>API对接方式</strong>: {{ $logistics->api_docking }}
+            </div>
+            <div class="col-lg-4">
+                <strong>是否启用</strong>: {{ $logistics->is_enable == 'Y' ? '是' : '否' }}
+            </div>
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">日志信息</div>
+        <div class="panel-body">
+            <div class="col-lg-6">
+                <strong>创建时间</strong>: {{ $logistics->created_at }}
+            </div>
+            <div class="col-lg-6">
+                <strong>更新时间</strong>: {{ $logistics->updated_at }}
+            </div>
         </div>
     </div>
 @stop

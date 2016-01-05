c55be75fc9e75677273a6164abdc4e200cf07c1c
Vincent <nyewon@gmail.com>
2016-01-05 15:47:11 +0800

    update

--- config/product.php
+++ config/product.php
@@ -1,12 +1,9 @@
 <?php
-    return [
-        'image'=>[
-			'types'=>['default','original','choies','aliexpress','amazon','ebay','wish','Lazada'],
-			'uploadPath'=>'product/',
-			'temporaryPath'=>'producttemporary/',
-			'uploadZip'=>'zip/'
-		],
-		'reportedMissing'=>[
-			'sizes'=>['','S','M','L','XL','XXL','XXXL','XXXXL'],
-		]
-    ];
+return [
+    'image' => [
+        'types' => ['default', 'original', 'choies', 'aliexpress', 'amazon', 'ebay', 'wish', 'Lazada'],
+        'uploadPath' => 'product/',
+        'temporaryPath' => 'producttemporary/',
+        'uploadZip' => 'zip/'
+    ],
+];

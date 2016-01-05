2e3a913aa052cd2dbae1e3b594e537fa438f0366
Vincent <nyewon@gmail.com>
2016-01-05 13:59:41 +0800

    update

--- app/Http/Controllers/Product/ImageController.php
+++ app/Http/Controllers/Product/ImageController.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * 图片控制器
  * 处理图片相关的Request与Response
@@ -22,8 +21,8 @@ class ImageController extends Controller
     public function __construct(Request $request, ImageRepository $image)
     {
         $this->request = $request;
-		$this->image =$image;
-		$this->mainIndex = route('productImage.index');
+        $this->image = $image;
+        $this->mainIndex = route('productImage.index');
         $this->mainTitle = '图片';
     }
 
@@ -36,14 +35,14 @@ class ImageController extends Controller
     {
         $this->request->flash();
         $response = [
-			'metas' => $this->metas(__FUNCTION__),
+            'metas' => $this->metas(__FUNCTION__),
             'data' => $this->image->auto()->paginate(),
         ];
 
         return view('product.image.index', $response);
     }
-	
-	
+
+
     /**
      * 图片详情
      *
@@ -51,15 +50,15 @@ class ImageController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function show($id)
-    {	
+    {
         $response = [
-			'metas' => $this->metas(__FUNCTION__),
-            'image' =>$this->image->get($id),
+            'metas' => $this->metas(__FUNCTION__),
+            'image' => $this->image->get($id),
         ];
         return view('product.image.show', $response);
     }
-	
-	/**
+
+    /**
      * 添加产品图片
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
@@ -67,26 +66,26 @@ class ImageController extends Controller
     public function create()
     {
         $response = [
-			'metas' => $this->metas(__FUNCTION__),
-            'imageType' =>  config('product.image.types'),
+            'metas' => $this->metas(__FUNCTION__),
+            'imageType' => config('product.image.types'),
         ];
         return view('product.image.create', $response);
     }
-	
-	 /**
+
+    /**
      * 图片上传
      *
-     * 
+     *
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function store()
     {
-       if($this->request->isMethod('post')){
-			$data=$this->request->all();
-			$files=$this->request->file();
-			$this->image->createImage($data,$files);	 
-		}
-        return redirect(route('productImage.index'));
+        if ($this->request->isMethod('post')) {
+            $data = $this->request->all();
+            $files = $this->request->file();
+            $this->image->createImage($data, $files);
+        }
+        return redirect($this->mainIndex);
     }
 
     /**
@@ -96,14 +95,14 @@ class ImageController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function edit($id)
-    { 
+    {
         $response = [
-			'metas' => $this->metas(__FUNCTION__),
-            'image' =>$this->image->get($id),
-			'imageType' =>  config('product.image.types'),
+            'metas' => $this->metas(__FUNCTION__),
+            'image' => $this->image->get($id),
+            'imageType' => config('product.image.types'),
         ];
-        return view('product.image.edit', $response); 
-    }	
+        return view('product.image.edit', $response);
+    }
 
     /**
      * 图片更新
@@ -112,13 +111,13 @@ class ImageController extends Controller
      */
     public function update()
     {
-        if($this->request->isMethod('post')){
-			$data=$this->request->all();
-			$file=$this->request->file('map');
-			$this->image->updateImage($data,$file);	 
-		}
-        return redirect(route('productImage.index'));
-    } 
+        if ($this->request->isMethod('post')) {
+            $data = $this->request->all();
+            $file = $this->request->file('map');
+            $this->image->updateImage($data, $file);
+        }
+        return redirect($this->mainIndex);
+    }
 
     /**
      * 删除
@@ -129,9 +128,9 @@ class ImageController extends Controller
     public function destroy($id)
     {
         $this->image->destroyImage($id);
-        return redirect(route('productImage.index'));
+        return redirect($this->mainIndex);
     }
-		
+
 }
 
 

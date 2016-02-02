ae20040dca0e20469379ee5c212259354376b1cb
youjia <youj@163.com>
2016-02-02 10:25:16 +0800

    1

--- database/migrations/2016_01_28_080255_create_products.php
+++ database/migrations/2016_01_28_080255_create_products.php
@@ -31,9 +31,9 @@ class CreateProducts extends Migration
             $table->string('purchase_carriage')->comment('运费')->nullable()->default(0);
             $table->string('product_size')->comment('产品尺寸')->nullable()->default(NUll);
             $table->string('carriage_limit')->comment('运费限制')->nullable()->default(NULL);
-            $table->string('carriage_limit1')->comment('运费限制1')->nullable()->default(NUll);
+            $table->string('carriage_limit_1')->comment('运费限制1')->nullable()->default(NUll);
             $table->string('package_limit')->comment('物流限制')->nullable()->default(NULL);
-            $table->string('package_limit1')->comment('物流限制1')->nullable()->default(NUll);
+            $table->string('package_limit_1')->comment('物流限制1')->nullable()->default(NUll);
             $table->string('package_size')->comment('包装尺寸')->nullable()->default(NULL);
             $table->string('upload_user')->comment('上传人')->nullable()->default(0);
             $table->string('default_image')->comment('默认图片')->nullable()->default(0);

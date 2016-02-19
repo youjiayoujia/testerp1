cf2b52c304676754a2432854a8ee7bd0e9937450
chenxuewen <852327204@qq.com>
2016-02-19 16:26:36 +0800

    update 渠道相关模块

--- app/Http/Controllers/Channel/AccountController.php
+++ app/Http/Controllers/Channel/AccountController.php
@@ -11,6 +11,8 @@ namespace App\Http\Controllers\Channel;
 use App\Http\Controllers\Controller;
 use App\Models\ChannelModel;
 use App\Models\Channel\AccountModel;
+use App\Models\CountryModel;
+use App\Models\UserModel;
 
 class AccountController extends Controller
 {
@@ -22,13 +24,28 @@ class AccountController extends Controller
         $this->viewPath = 'channel.account.';
     }
 
+
     public function create()
     {
         $response = [
             'metas' => $this->metas(__FUNCTION__),
             'channels' => ChannelModel::all(),
+            'users' => UserModel::orderBy('name', 'asc')->get(['id', 'name']),
+            'countries'=>CountryModel::orderBy('abbreviation', 'asc')->get(['id', 'name'])
         ];
         return view($this->viewPath . 'create', $response);
+    }
+
+
+    public function store()
+    {
+        request()->flash();
+        $this->validate(request(), $this->model->rules('create'));
+        $businesserIds =  request()->input("businesser_ids");
+        $businesserArray = explode(',' ,$businesserIds);
+        $channel = AccountModel::create(request()->all());
+        $channel->businessers()->sync($businesserArray);
+        return redirect($this->mainIndex);
     }
 
     public function edit($id)
@@ -39,9 +56,26 @@ class AccountController extends Controller
         }
         $response = [
             'metas' => $this->metas(__FUNCTION__),
-            'channels' => ChannelModel::all(),
             'model' => $account,
+            'channels' => ChannelModel::all(),
+            'users' => UserModel::orderBy('name', 'asc')->get(['id', 'name']),
+            'countries'=>CountryModel::orderBy('abbreviation', 'asc')->get(['id', 'name'])
         ];
         return view($this->viewPath . 'edit', $response);
+    }
+
+    public function update($id)
+    {
+        $model = $this->model->find($id);
+        if (!$model) {
+            return redirect($this->mainIndex)->with('alert', $this->alert('danger', $this->mainTitle . '不存在.'));
+        }
+        request()->flash();
+        $this->validate(request(), $this->model->rules('update', $id));
+        $businesserIds =  request()->input("businesser_ids");
+        $businesserArray = explode(',' ,$businesserIds);
+        $model->update(request()->all());
+        $model->businessers()->sync($businesserArray);
+        return redirect($this->mainIndex);
     }
 }
--- app/Views/channel/create.blade.php
+++ app/Views/channel/create.blade.php
@@ -6,8 +6,8 @@
         <small class="text-danger glyphicon glyphicon-asterisk"></small>
         <input type='text' class="form-control" id="name" placeholder="渠道英文名称" name='name' value="{{ old('name') }}">
     </div>
-    <div class="form-group col-lg-6">
-        <label for="alias" class='control-label'>渠道别称</label>
-        <input type='text' class="form-control" id="alias" placeholder="渠道中文名称" name='alias' value="{{ old('alias') }}">
+    <div class="form-group col-lg-12">
+        <label for="brief" class='control-label'>描述</label>
+        <textarea class="form-control" rows="3" name="brief">{{ old('brief') }}</textarea>
     </div>
 @stop

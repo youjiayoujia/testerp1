2213c27b3c42c4a840611106558438e446ce2bf2
Vincent <nyewon@gmail.com>
2016-02-25 16:47:14 +0800

    help provider

--- app/Providers/HelpServiceProvider.php
+++ app/Providers/HelpServiceProvider.php
@@ -26,5 +26,9 @@ class HelpServiceProvider extends ServiceProvider
         $this->app->singleton('datalist', function () {
             return new \App\Helps\DataList;
         });
+
+        $this->app->singleton('tool', function () {
+            return new \App\Helps\Tool;
+        });
     }
 }

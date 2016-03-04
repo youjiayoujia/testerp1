8c2baa52a81d36196bdc76eed0d6fe3bae0149a7
Vincent <nyewon@gmail.com>
2016-03-04 12:49:35 +0800

    no message

--- app/Models/Channel/AccountModel.php
+++ app/Models/Channel/AccountModel.php
@@ -24,49 +24,47 @@ class AccountModel extends BaseModel
      * @var array
      */
     protected $fillable = [
-        'name',
+        'account',
         'alias',
         'channel_id',
         'country_id',
         'domain',
         'sync_cycle',
-        'activate',
-        'default_businesser_id',
-        'default_server_id',
-        'email',
-        'delivery_warehouse',
-        'merge_package',
-        'thanks',
-        'picking_list',
-        'generate_sku',
-        'image_site',
-        'clearance',
+        'is_available',
+        'operator_id',
+        'customer_service_id',
+        'service_email',
+        'warehouse_id',
+        'is_merge_package',
+        'is_thanks',
+        'is_picking_list',
+        'is_rand_sku',
+        'image_domain',
+        'is_clearance',
         'tracking_config',
         'order_prefix',
     ];
 
-    public $searchFields = ['name', 'alias', 'merge_package', 'thanks', 'picking_list'];
+    public $searchFields = ['account', 'alias'];
 
     protected $rules = [
         'create' => [
-            'name' => 'required',
+            'account' => 'required',
             'alias' => 'required',
             'channel_id' => 'required',
             'country_id' => 'required',
-            'default_businesser_id' => 'required',
-            'default_server_id' => 'required',
-            'email' => 'required|email',
-            'delivery_warehouse' => 'required|max:255',
+            'operator_id' => 'required',
+            'customer_service_id' => 'required',
+            'warehouse_id' => 'required|max:255',
         ],
         'update' => [
-            'name' => 'required',
+            'account' => 'required',
             'alias' => 'required',
             'channel_id' => 'required',
             'country_id' => 'required',
-            'default_businesser_id' => 'required',
-            'default_server_id' => 'required',
-            'email' => 'required|email',
-            'delivery_warehouse' => 'required|max:255',
+            'operator_id' => 'required',
+            'customer_service_id' => 'required',
+            'warehouse_id' => 'required|max:255',
         ]
     ];
 
@@ -81,19 +79,70 @@ class AccountModel extends BaseModel
     }
 
 
-    public function businessers()
+    public function operators()
     {
-        return $this->belongsToMany('App\Models\UserModel', 'channel_account_user', 'channel_account_id', 'user_id')->withTimestamps();
+        return $this->belongsToMany('App\Models\UserModel', 'channel_account_operators',
+            'channel_account_id', 'user_id');
     }
 
-    public function default_businesser()
+    public function operator()
     {
-        return $this->belongsTo('App\Models\UserModel', 'default_businesser_id', 'id');
+        return $this->belongsTo('App\Models\UserModel', 'operator_id', 'id');
     }
 
-    public function default_server()
+    public function customer_service()
     {
-        return $this->belongsTo('App\Models\UserModel', 'default_server_id', 'id');
+        return $this->belongsTo('App\Models\UserModel', 'customer_service_id', 'id');
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo('App\Models\WarehouseModel', 'warehouse_id', 'id');
+
+    }
+
+    public function createAccount()
+    {
+        $channel = $this->create(request()->all());
+        $operatorIds = explode(',', request()->input("operator_ids"));
+        $channel->operators()->attach($operatorIds);
+        return $channel;
+    }
+
+    public function destoryAccount()
+    {
+        $this->operators()->detach();
+        $this->delete();
+    }
+
+    public function getMergePackageAttribute()
+    {
+        return $this->is_merge_package ? '是' : '否';
+    }
+
+    public function getThanksAttribute()
+    {
+        return $this->is_thanks ? '是' : '否';
+    }
+
+    public function getPickingListAttribute()
+    {
+        return $this->is_picking_list ? '是' : '否';
+    }
+
+    public function getRandSkuAttribute()
+    {
+        return $this->is_rand_sku ? '是' : '否';
+    }
+
+    public function getClearanceAttribute()
+    {
+        return $this->is_clearance ? '是' : '否';
+    }
+
+    public function getAvailableAttribute()
+    {
+        return $this->is_available ? '是' : '否';
     }
 
 }
--- database/migrations/2016_01_04_083108_create_channel_accounts.php
+++ database/migrations/2016_01_04_083108_create_channel_accounts.php
@@ -14,40 +14,25 @@ class CreateChannelAccounts extends Migration
     {
         Schema::create('channel_accounts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->comment('渠道账号')->default("");
-            $table->string('alias')->comment('渠道账号别名')->default("");
+            $table->string('account')->comment('渠道账号');
+            $table->string('alias')->comment('渠道账号别名');
             $table->integer('channel_id')->comment('渠道');
             $table->integer('country_id')->comment('所在国家');
-            $table->string('domain')->comment('账号对应域名')->default("");
-            $table->integer('sync_cycle')->comment('订单同步周期')->default("1");
-            $table->string('activate')->comment('是否激活')->default("N");
-
-            $table->integer('default_businesser_id')->comment('默认运营人员');
-            $table->integer('default_server_id')->comment('默认客服人员');
-
-            $table->string('email')->comment('客服邮箱地址')->default("");
-            $table->string('delivery_warehouse')->comment('默认发货仓库')->default("");
-            $table->string('merge_package')->comment('合并相同地址订单包裹:')->default("N");
-            $table->string('thanks')->comment('是否打印感谢信:')->default("N");
-            $table->string('picking_list')->comment('是否打印拣货单')->default("N");
-            $table->string('generate_sku')->comment('是否无规则生成渠道SKU')->default("N");
-            $table->string('image_site')->comment('产品图片域名')->default("");
-            $table->string('clearance')->comment('可否通关')->default("N");
-            $table->string('tracking_config')->comment('上传追踪号配置')->default("");
-            $table->string('order_prefix')->comment('订单前缀')->default("");
-
-            $table->timestamps();
-            $table->softDeletes();
-        });
-
-        Schema::create('channel_account_user', function (Blueprint $table) {
-
-            $table->integer('channel_account_id')->unsigned()->index();
-            $table->foreign('channel_account_id')->references('id')->on('channel_accounts')->onDelete('cascade');
-
-            $table->integer('user_id')->unsigned()->index();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-
+            $table->string('order_prefix')->comment('订单前缀');
+            $table->float('sync_cycle')->comment('订单同步周期');
+            $table->string('domain')->comment('账号对应域名');
+            $table->string('image_domain')->comment('产品图片域名');
+            $table->string('service_email')->comment('客服邮箱地址');
+            $table->string('tracking_config')->comment('上传追踪号配置');
+            $table->integer('operator_id')->comment('默认运营人员');
+            $table->integer('customer_service_id')->comment('默认客服人员');
+            $table->integer('warehouse_id')->comment('默认发货仓库');
+            $table->enum('is_merge_package', ['0', '1'])->comment('合并相同地址订单包裹')->default('0');
+            $table->enum('is_thanks', ['0', '1'])->comment('是否打印感谢信')->default('0');
+            $table->enum('is_picking_list', ['0', '1'])->comment('是否打印拣货单')->default('0');
+            $table->enum('is_rand_sku', ['0', '1'])->comment('是否随机生成渠道SKU')->default('0');
+            $table->enum('is_clearance', ['0', '1'])->comment('可否通关')->default('0');
+            $table->enum('is_available', ['0', '1'])->comment('是否激活')->default('0');
             $table->timestamps();
             $table->softDeletes();
         });

6e910416efb827dc3dae0cb74b932a49ba1e4cb2
youjia <youj@163.com>
2016-03-09 15:55:29 +0800

    迁移文件

--- database/migrations/2016_03_04_091843_create_amazon_product.php
+++ database/migrations/2016_03_04_091843_create_amazon_product.php
@@ -15,8 +15,19 @@ class CreateAmazonProduct extends Migration
         Schema::create('amazon_products', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id')->comment('产品ID')->nullable()->default(null);
-            $table->integer('channel_id')->comment('渠道ID')->nullable()->default(null);
             $table->string('choice_info')->comment('平台信息')->nullable()->default(null);
+            $table->string('name')->comment('产品name')->nullable()->default(null);
+            $table->string('c_name')->comment('产品中文name')->nullable()->default(null);
+            $table->integer('supplier_id')->comment('供应商')->nullable()->default(null);
+            $table->string('supplier_info')->comment('供应商信息')->nullable()->default(null);
+            $table->string('supplier_sku')->comment('供应商sku')->nullable()->default(null);
+            $table->string('product_sale_url')->comment('供应商sku对应的url')->nullable()->default(null);
+            $table->float('purchase_price')->comment('采购价')->nullable()->default(null);
+            $table->float('purchase_carriage')->comment('物流费')->nullable()->default(null);
+            $table->float('weight')->comment('重量')->nullable()->default(null);
+            $table->string('remark')->comment('备注')->nullable()->default(null);
+            $table->string('image_remark')->comment('图片备注')->nullable()->default(null);
+            $table->tinyInteger('status')->comment('平台信息')->nullable()->default(null);
             $table->timestamps();
             $table->softDeletes();
         });

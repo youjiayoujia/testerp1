4798dbb2105d5b4ab3e185f8553319bf05d5f4a7
Vincent <nyewon@gmail.com>
2016-03-11 09:54:45 +0800

    package

--- app/Models/OrderModel.php
+++ app/Models/OrderModel.php
@@ -17,21 +17,62 @@ class OrderModel extends BaseModel
     protected $table = 'orders';
 
     protected $fillable = [
-        'channel_id','channel_account_id','order_number',
-        'channel_order_number','email','status','active','amount','amount_product',
-        'amount_shipping','amount_coupon','is_partial','by_hand','is_affair','affairer',
-        'customer_service','operator','payment','currency','rate','ip','address_confirm',
-        'comment','comment1','remark','import_remark','shipping','shipping_firstname',
-        'shipping_lastname','shipping_address','shipping_address1','shipping_city',
-        'shipping_state','shipping_country','shipping_zipcode','shipping_phone',
-        'billing_firstname','billing_lastname','billing_address','billing_city',
-        'billing_state','billing_country','billing_zipcode','billing_phone',
-        'payment_date','affair_time','create_time',
+        'channel_id',
+        'channel_account_id',
+        'order_number',
+        'channel_order_number',
+        'email',
+        'status',
+        'active',
+        'amount',
+        'amount_product',
+        'amount_shipping',
+        'amount_coupon',
+        'is_partial',
+        'by_hand',
+        'is_affair',
+        'affairer',
+        'customer_service',
+        'operator',
+        'payment',
+        'currency',
+        'rate',
+        'ip',
+        'address_confirm',
+        'comment',
+        'comment1',
+        'remark',
+        'import_remark',
+        'shipping',
+        'shipping_firstname',
+        'shipping_lastname',
+        'shipping_address',
+        'shipping_address1',
+        'shipping_city',
+        'shipping_state',
+        'shipping_country',
+        'shipping_zipcode',
+        'shipping_phone',
+        'billing_firstname',
+        'billing_lastname',
+        'billing_address',
+        'billing_city',
+        'billing_state',
+        'billing_country',
+        'billing_zipcode',
+        'billing_phone',
+        'payment_date',
+        'affair_time',
+        'create_time',
     ];
 
     public $searchFields = [
-        'channel_id', 'channel_account_id', 'order_number',
-        'email', 'customer_service', 'operator',
+        'channel_id',
+        'channel_account_id',
+        'order_number',
+        'email',
+        'customer_service',
+        'operator',
     ];
 
     public $rules = [
@@ -64,42 +105,44 @@ class OrderModel extends BaseModel
         $arr = [];
         $buf = $request->all();
         $buf = $buf['arr'];
-        foreach($buf as $key => $val)
-        {
-            if($key == 'sku')
-                foreach($val as $k => $v)
-                {
-                    $arr['arr.sku.'.$k] ='required';
+        foreach ($buf as $key => $val) {
+            if ($key == 'sku') {
+                foreach ($val as $k => $v) {
+                    $arr['arr.sku.' . $k] = 'required';
                 }
-            if($key == 'qty')
-                foreach($val as $k => $v)
-                {
-                    $arr['arr.qty.'.$k] ='required';
+            }
+            if ($key == 'qty') {
+                foreach ($val as $k => $v) {
+                    $arr['arr.qty.' . $k] = 'required';
                 }
-            if($key == 'price')
-                foreach($val as $k => $v)
-                {
-                    $arr['arr.price.'.$k] ='required';
+            }
+            if ($key == 'price') {
+                foreach ($val as $k => $v) {
+                    $arr['arr.price.' . $k] = 'required';
                 }
-            if($key == 'is_gift')
-                foreach($val as $k => $v)
-                {
-                    $arr['arr.is_gift.'.$k] = 'required';
+            }
+            if ($key == 'is_gift') {
+                foreach ($val as $k => $v) {
+                    $arr['arr.is_gift.' . $k] = 'required';
                 }
+            }
         }
 
         return $arr;
     }
 
-    public function orderItem() {
+    public function items()
+    {
         return $this->hasMany('App\Models\Order\ItemModel', 'order_id', 'id');
     }
 
-    public function channel() {
+    public function channel()
+    {
         return $this->belongsTo('App\Models\ChannelModel', 'channel_id', 'id');
     }
 
-    public function channelAccount() {
+    public function channelAccount()
+    {
         return $this->belongsTo('App\Models\Channel\AccountModel', 'channel_account_id', 'id');
     }
 

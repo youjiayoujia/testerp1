e47a6b8b96988f39440d3acacfdb030e5fedceea
卞海伟 <bianhaiwei@bianhaiweideMacBook-Pro.local>
2016-03-18 17:00:53 +0800

    Revert "no message"
    This reverts commit 4bcc1bca07fee69ed5c6bd43773c84b87e0b85d2.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -136,4 +136,3 @@ Route::resource('orderItem', 'Order\ItemController');
 Route::get('orderAdd', ['uses' => 'OrderController@ajaxOrderAdd', 'as' => 'orderAdd']);
 Route::get('account', ['uses' => 'OrderController@account', 'as' => 'account']);
 Route::get('getMsg', ['uses' => 'OrderController@getMsg', 'as' => 'getMsg']);
-Route::get('getChoiesOrder',['uses' => 'OrderController@getChoiesOrder', 'as' => 'getChoiesOrder']);

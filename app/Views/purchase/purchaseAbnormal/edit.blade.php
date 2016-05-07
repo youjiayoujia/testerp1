013a08e899dc5bde16bf0ad4d2b694b8ecc6c804
Vincent <nyewon@gmail.com>
2016-05-07 17:22:07 +0800

    no message

--- app/Views/purchase/purchaseAbnormal/edit.blade.php
+++ app/Views/purchase/purchaseAbnormal/edit.blade.php
@@ -23,22 +23,29 @@
     </div>
     <div class="row">
 
-        @if($model->active == 1)
-            <div class="form-group col-lg-4">
-                <label for="sku_id">辅助供应商:</label>
-                <input type="radio" name="supplier_id" value="{{$secondSupplier->id}}">名：{{$secondSupplier->name}}&nbsp;电话：{{$secondSupplier->telephone}}
-                &nbsp;地址：{{$secondSupplier->province}}{{$secondSupplier->city}}{{$secondSupplier->address}}
-            </div>
-        @elseif($model->active == 2)
-            <div class="form-group col-lg-4">
-                <label for="sku_id">预计报等时间:</label>
-                {{$model->wait_time}}
-            </div>
-            <div class="form-group col-lg-4">
-                <label for="sku_id">报等备注:</label>
-                {{$model->wait_remark}}
-            </div>
-        @endif
+        <div class="form-group col-lg-4">
+            <label class='control-label'>异常状态:</label>
+            <select name="active_status">
+                @if($model->active == 1)
+                    @foreach(config('purchase.purchaseItem.active_status.1') as $key=>$v)
+                        <option {{$model->active_status == $key ? 'selected' : ''}} value="{{$key}}">{{$v}}</option>
+                    @endforeach
+                @elseif($model->active == 2)
+                    @foreach(config('purchase.purchaseItem.active_status.2') as $key=>$v)
+                        <option {{$model->active_status == $key ? 'selected' : ''}} value="{{$key}}">{{$v}}</option>
+                    @endforeach
+                @elseif($model->active == 3)
+                    @foreach(config('purchase.purchaseItem.active_status.3') as $key=>$v)
+                        <option {{$model->active_status == $key ? 'selected' : ''}} value="{{$key}}">{{$v}}</option>
+                    @endforeach
+                @elseif($model->active == 4)
+                    @foreach(config('purchase.purchaseItem.active_status.4') as $key=>$v)
+                        <option {{$model->active_status == $key ? 'selected' : ''}} value="{{$key}}">{{$v}}</option>
+                    @endforeach
+                @endif
+            </select>
+        </div>
+        
     </div>
 @stop
  

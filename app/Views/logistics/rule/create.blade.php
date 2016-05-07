714d57c3fd43e58f26c5e3a92122f2f28c52ee0d
卞海伟 <bianhaiwei@bianhaiweideMacBook-Pro.local>
2016-05-07 15:38:21 +0800

    no message

--- app/Views/logistics/rule/create.blade.php
+++ app/Views/logistics/rule/create.blade.php
@@ -56,7 +56,7 @@
             <small class="text-danger glyphicon glyphicon-asterisk"></small>
             <select name="country" class="form-control" multiple style="height:300px;width:400px;">
                 @foreach($countries as $country)
-                    <option class="form-control" value="{{ $country->abbreviation }}" {{ old('country') ? old('country') == $country->abbreviation ? 'selected' : '' : ''}} onclick="addCountry( this )">
+                    <option class="form-control" value="{{ $country->code }}" {{ old('country') ? old('country') == $country->code ? 'selected' : '' : ''}} onclick="addCountry( this )">
                         {{ $country->name }}
                     </option>
                 @endforeach

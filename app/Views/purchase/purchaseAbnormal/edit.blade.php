1b4e058261e85dabc7e0d5865a2a66e04b225cf5
Vincent <nyewon@gmail.com>
2016-05-07 17:27:33 +0800

    no message

--- app/Views/purchase/purchaseAbnormal/edit.blade.php
+++ app/Views/purchase/purchaseAbnormal/edit.blade.php
@@ -3,6 +3,24 @@
 @section('formBody')
     <input type="hidden" name="_method" value="PUT"/>
     <input type="hidden" name="update_userid" value="2"/>
+    <div class="row">
+        <div class="form-group col-lg-4">
+            <label for="warehouse">仓库:</label>
+            {{$model->warehouse->name}}
+        </div>
+        <div class="form-group col-lg-4">
+            <label for="sku_id">sku:</label>
+            {{$model->sku}}
+        </div>
+        <div class="form-group col-lg-4">
+            <label for="type">异常种类：</label>
+            @foreach(config('purchase.purchaseItem.active') as $k=>$active)
+                @if($model->active == $k)
+                    <td>{{ $active }}</td>
+                @endif
+            @endforeach
+        </div>
+    </div>
 @stop
  
  

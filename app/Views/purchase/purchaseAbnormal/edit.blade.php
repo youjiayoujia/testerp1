0ee22c422856d14d216b23dead705202e59a2d62
Vincent <nyewon@gmail.com>
2016-05-07 17:19:45 +0800

    no message

--- app/Views/purchase/purchaseAbnormal/edit.blade.php
+++ app/Views/purchase/purchaseAbnormal/edit.blade.php
@@ -6,7 +6,6 @@
     <div class="row">
         <div class="form-group col-lg-4">
             <label for="warehouse">仓库:</label>
-{{--            {{$model->warehouse->name}}--}}
         </div>
         <div class="form-group col-lg-4">
             <label for="sku_id">sku:</label>

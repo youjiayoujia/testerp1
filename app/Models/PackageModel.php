57a3b0cff24c91ff1f6098d511dd14047386ed05
Vincent <nyewon@gmail.com>
2016-05-09 15:31:11 +0800

    no message

--- app/Models/PackageModel.php
+++ app/Models/PackageModel.php
@@ -171,7 +171,8 @@ class PackageModel extends BaseModel
                 if ($this->shipping_limits->intersect($limits)->count() > 0) {
                     continue;
                 }
-                //物流商下单
+                echo 'here'.$rule->id;
+                //物流查询链接
                 $trackingUrl = $rule->logistics->url;
                 return $this->update([
                     'status' => 'ASSIGNED',

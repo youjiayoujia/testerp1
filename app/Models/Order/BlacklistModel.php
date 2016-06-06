f9c61c0acc53dd4a54c3e2fad7d9b7dee58a94c3
卞海伟 <bianhaiwei@bianhaiweideMacBook-Pro.local>
2016-06-06 18:04:30 +0800

    no message

--- app/Models/Order/BlacklistModel.php
+++ app/Models/Order/BlacklistModel.php
@@ -69,7 +69,6 @@ class BlacklistModel extends BaseModel
         $rows = '';
         foreach($all as $model) {
             $rows[] = [
-                'id' => $model->id,
                 'channel_id' => $model->channel_id,
                 'ordernum' => $model->ordernum,
                 'name' => $model->name,
@@ -92,12 +91,12 @@ class BlacklistModel extends BaseModel
         foreach($part as $model) {
             $rows[] = [
                 'id' => $model->id,
-                'channel_id' => $model->channel_id,
+                'channel_id' => $model->channel->name,
                 'ordernum' => $model->ordernum,
                 'name' => $model->name,
                 'email' => $model->email,
                 'zipcode' => $model->zipcode,
-                'type' => $model->type,
+                'type' => $model->type_name,
                 'remark' => $model->remark,
                 'total_order' => $model->total_order,
                 'refund_order' => $model->refund_order,
@@ -105,6 +104,75 @@ class BlacklistModel extends BaseModel
             ];
         }
         return $rows;
+    }
+
+    /**
+     * 处理excel
+     */
+    public function excelProcess($file, $name)
+    {
+        $path = config('order.excelPath');
+        !file_exists($path.'excelProcess.xls') or unlink($path.'excelProcess.xls');
+        $file->move($path, 'excelProcess.xls');
+        return $this->$name($path.'excelProcess.xls');
+    }
+
+    /**
+     * 处理excel数据
+     */
+    public function excelBlacklistProcess($path)
+    {
+        $fd = fopen($path, 'r');
+        $arr = [];
+        while(!feof($fd))
+        {
+            $row = fgetcsv($fd);
+            $arr[] = $row;
+        }
+        fclose($fd);
+        if(!$arr[count($arr)-1]) {
+            unset($arr[count($arr)-1]);
+        }
+        $arr = $this->transfer_arr($arr);
+        $error[] = $arr;
+        foreach($arr as $key=> $blacklist)
+        {
+            $blacklist['channel_id'] = iconv('gb2312','utf-8',$blacklist['channel_id']);
+            $blacklist['ordernum'] = iconv('gb2312','utf-8',$blacklist['ordernum']);
+            $blacklist['name'] = iconv('gb2312','utf-8',$blacklist['name']);
+            $blacklist['email'] = iconv('gb2312','utf-8',$blacklist['email']);
+            $blacklist['zipcode'] = iconv('gb2312','utf-8',$blacklist['zipcode']);
+            $blacklist['type'] = iconv('gb2312','utf-8',$blacklist['type']);
+            $blacklist['remark'] = iconv('gb2312','utf-8',$blacklist['remark']);
+            $blacklist['total_order'] = iconv('gb2312','utf-8',$blacklist['total_order']);
+            $blacklist['refund_order'] = iconv('gb2312','utf-8',$blacklist['refund_order']);
+            $blacklist['refund_rate'] = iconv('gb2312','utf-8',$blacklist['refund_rate']);
+//            if($this->where('channel_id', $blacklist['channel_id'])->count()) {
+//                $error[] = $key;
+//                continue;
+//            }
+            $this->create($blacklist);
+        }
+
+        return $error;
+    }
+
+    public function transfer_arr($arr)
+    {
+        $buf = [];
+        foreach($arr as $key => $value)
+        {
+            $tmp = [];
+            if($key != 0) {
+                foreach($value as $k => $v)
+                {
+                    $tmp[$arr[0][$k]] = $v;
+                }
+                $buf[] = $tmp;
+            }
+        }
+
+        return $buf;
     }
 
 }

2bbb076b1fc09d1aa3af1bf34aa8b981fecc310a
Vincent <nyewon@gmail.com>
2016-06-24 18:05:03 +0800

    update

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -20,7 +20,7 @@ class DataController extends Controller
 
     public function index()
     {
-        $smProducts = smProduct::limit(100)->get();
+        $smProducts = smProduct::limit(100)->orderBy('products_id', 'desc')->get();
         foreach ($smProducts as $smProduct) {
             $item = [
                 'catalog_id' => 0,
@@ -33,18 +33,18 @@ class DataController extends Controller
                 'warehouse_position' => $smProduct->products_location,
 //                'alias_name' => $smProduct->products_declared_en,
 //                'alias_cname' => $smProduct->products_declared_cn,
-                'supplier_id' => SupplierModel::where('old_id', $smProduct->products_suppliers_id)->get()->id,
-                'supplier_sku' => '',
-                'second_supplier_id' => function ($smProduct) {
-                    $suppliers = explode(',', $smProduct->products_suppliers_id);
-                    if (isset($suppliers[1])) {
-                        $secondSupplier = SupplierModel::where('old_id', $suppliers[1])->get();
-                        if ($secondSupplier) {
-                            return $secondSupplier->id;
-                        }
-                    }
-                    return '';
-                },
+//                'supplier_id' => SupplierModel::where('old_id', $smProduct->products_suppliers_id)->get()->id,
+//                'supplier_sku' => '',
+//                'second_supplier_id' => function ($smProduct) {
+//                    $suppliers = explode(',', $smProduct->products_suppliers_id);
+//                    if (isset($suppliers[1])) {
+//                        $secondSupplier = SupplierModel::where('old_id', $suppliers[1])->get();
+//                        if ($secondSupplier) {
+//                            return $secondSupplier->id;
+//                        }
+//                    }
+//                    return '';
+//                },
                 'second_supplier_sku' => '',
                 'supplier_info' => $smProduct->products_sku,
                 'purchase_url' => $smProduct->productsPhotoStandard,
@@ -56,19 +56,22 @@ class DataController extends Controller
                         $volumes = unserialize($smProduct->products_volume);
                         return $volumes['bp']['length'] . '*' . $volumes['bp']['width'] . '*' . $volumes['bp']['height'];
                     }
+                    return '';
                 },
                 'package_size' => function ($smProduct) {
                     if ($smProduct->products_volume) {
                         $volumes = unserialize($smProduct->products_volume);
                         return $volumes['ap']['length'] . '*' . $volumes['ap']['width'] . '*' . $volumes['ap']['height'];
                     }
+                    return '';
                 },
                 'carriage_limit' => '',
                 'package_limit' => '',
                 'status' => $smProduct->products_status_2,
-                'is_sale' => $smProduct->products_sku,
-                'remark' => $smProduct->products_sku,
+                'is_sale' => $smProduct->productsIsActive,
+                'remark' => $smProduct->products_remark,
             ];
+            ItemModel::create($item);
         }
     }
 }

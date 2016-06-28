66bcf176c04e13c855a5fae0fef8ee0d2214e4e9
Vincent <nyewon@gmail.com>
2016-06-28 19:41:22 +0800

    update

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -375,6 +375,7 @@ class DataController extends Controller
     public function __construct(smProduct $model)
     {
         set_time_limit(0);
+        ini_set('memory_limit', '256M');
     }
 
     public function index()

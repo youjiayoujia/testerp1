516d9223d04222614071aac0530b9f8685e28975
Vincent <nyewon@gmail.com>
2016-07-01 16:21:31 +0800

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,12 @@
  */
 
 // Authentication routes...
-Route::any('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 // Registration routes...
-Route::any('auth/register', 'Auth\AuthController@getRegister');
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 Route::group(['middleware' => 'auth'], function () {
     //Home

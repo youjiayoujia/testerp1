79c198cdbed6d6dfab1d9b42424ae20e8faffc1f
mcss <178069409@qq.com>
2016-07-04 10:15:56 +0800

    no message

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -56,7 +56,9 @@ class PackageController extends Controller
             'metas' => $this->metas(__FUNCTION__, 'Flow'),
             'packageNum' => OrderModel::where('active', 'NORMAL')
                 ->whereIn('status', ['NEED'])->count(),
-            'manualShip' => $this->model->where(['is_auto' => '0'])->count(),
+            'assignNum' => $this->model->where(['status' => 'NEW'])->count(),
+            'placeNum' => $this->model->where(['status' => 'ASSIGNED'])->count(),
+            'manualShip' => $this->model->where(['is_auto' => '0', 'status' => 'ASSIGNED'])->count(),
             'pickNum' => $this->model->where(['status' => 'PROCESSING', 'is_auto' => '1'])->count(),
             'printNum' => PickListModel::where('status', 'NONE')->count(),
             'singlePack' => PickListModel::where('type', 'SINGLE')->whereIn('status',

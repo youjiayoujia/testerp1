db2d51a4f2b7c02e229dca49e3811227c25c500e
youjia <youj@163.com>
2016-07-09 15:14:22 +0800

    1

--- app/Views/role/edit.blade.php
+++ app/Views/role/edit.blade.php
@@ -4,14 +4,14 @@
     <input type="hidden" name="_method" value="PUT"/>
     <div class="row">
         <div class="form-group col-lg-6">
-            <label for="role" class='control-label'>角色</label>
+            <label for="role_name" class='control-label'>角色</label>
             <small class="text-danger glyphicon glyphicon-asterisk"></small>
-            <input type='text' class="form-control" id="role" placeholder="角色" name='role' value="{{ $model->role }}">
+            <input type='text' class="form-control" id="role_name" placeholder="角色" name='role_name' value="{{ $model->role_name }}">
         </div>
         <div class="form-group col-lg-6">
-            <label for="role_name" class='control-label'>role_name</label>
+            <label for="role" class='control-label'>role</label>
             <small class="text-danger glyphicon glyphicon-asterisk"></small>
-            <input type='text' class="form-control" id="role_name" placeholder="role_name" name='role_name' value="{{ $model->role_name }}">
+            <input type='text' class="form-control" id="role" placeholder="role" name='role' value="{{ $model->role }}">
         </div>
     </div>
 

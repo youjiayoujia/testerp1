c253704ec270179622f992f1b8a54e78035bc5e6
卞海伟 <bianhaiwei@bianhaiweideMacBook-Pro.local>
2016-08-10 14:33:44 +0800

    no message

--- app/Views/logistics/index.blade.php
+++ app/Views/logistics/index.blade.php
@@ -10,6 +10,7 @@
     <th>对接方式</th>
     <th class="sort" data-field="pool_quantity">号码池数量(未用/已用/总数)</th>
     <th>物流限制</th>
+    <th>物流编码</th>
     <th>是否启用</th>
     <th class="sort" data-field="created_at">创建时间</th>
     <th class="sort" data-field="updated_at">更新时间</th>
@@ -28,6 +29,7 @@
             <td>{{ $logistics->docking_name }}</td>
             <td>{{ $logistics->pool_quantity }}</td>
             <td>{{ $logistics->limit($logistics->limit) }}</td>
+            <td>{{ $logistics->logistics_code }}</td>
             <td>{{ $logistics->is_enable == '1' ? '是' : '否' }}</td>
             <td>{{ $logistics->created_at }}</td>
             <td>{{ $logistics->updated_at }}</td>

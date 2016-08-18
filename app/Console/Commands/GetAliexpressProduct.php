891b602fed949c724c047792ab233e8c18ed89d7
guoou <916091060@qq.com>
2016-08-18 13:17:59 +0800

    aaa

--- app/Console/Commands/GetAliexpressProduct.php
+++ app/Console/Commands/GetAliexpressProduct.php
@@ -8,7 +8,6 @@ use App\Models\Channel\AccountModel;
 use App\Models\Publish\Smt\smtProductSku;
 use App\Models\Publish\Smt\smtProductDetail;
 use App\Models\Publish\Smt\smtProductList;
-use App\Models\ChannelModel;
 class GetAliexpressProduct extends Command
 {
     /**
@@ -16,8 +15,8 @@ class GetAliexpressProduct extends Command
      *
      * @var string
      */
-   // protected $signature = 'smtProduct:do {accountID}';
-    protected $signature = 'smtProduct:do';
+    protected $signature = 'smtProduct:do {accountID}';
+
     /**
      * The console command description.
      *
@@ -42,98 +41,8 @@ class GetAliexpressProduct extends Command
      */
     public function handle()
     {
-        //$account = AccountModel::find($this->argument('accountID'));
-        $this->channel_id = ChannelModel::where('driver','aliexpress')->first()->id;
-        $accountArr = AccountModel::where('channel_id',$this->channel_id)->get();
-        $channel = Channel::driver($account->channel->driver, $account->api_config);
-        $flag = true;
-        $start = microtime(true);
-        $currentPage = 1;
-        $productDetail = array();
-        $product = array();
-        $productSKU = array();
-        foreach($accountArr as $account){            
-            while($flag){
-                $productList = $channel->getOnlineProduct($currentPage, 100);
-                if(array_key_exists('success', $productList) && $productList['success']){
-                    foreach($productList['aeopAEProductDisplayDTOList'] as $productItem) {
-                        $productInfo = $channel->findAeProductById($productItem['productId']);
-                        $product['productId'] = $productItem['productId'];
-                        $product['token_id'] = $account->id;
-                        $product['subject'] = $productInfo['subject'];
-                        $product['productPrice'] = $productInfo['productPrice'];
-                        $product['productStatusType'] = $productInfo['productStatusType'];
-                        $product['ownerMemberId'] = $productInfo['ownerMemberId'];
-                        $product['ownerMemberSeq'] = $productInfo['ownerMemberSeq'];
-                        $product['wsOfflineDate'] = $productInfo['wsOfflineDate'];
-                        $product['groupId'] = array_key_exists('groupId', $productInfo) ? $productInfo['groupId'] : '';
-                        $product['categoryId'] = $productInfo['categoryId'];
-                        $product['packageLength'] = $productInfo['packageLength'];
-                        $product['packageWidth'] = $productInfo['packageWidth'];
-                        $product['packageHeight'] = $productInfo['packageHeight'];
-                        $product['grossWeight'] = $productInfo['grossWeight'];
-                        $product['deliveryTime'] = $productInfo['deliveryTime'];
-                        $product['wsValidNum'] = $productInfo['wsValidNum'];
-                        $product['productMinPrice'] = $productItem['productMinPrice'];
-                        $product['productMaxPrice'] = $productItem['productMaxPrice'];
-                        $productItem['gmtCreate'] = $productItem['gmtCreate'];
-                        $productItem['gmtModified'] = $productItem['gmtModified'];
-                        $res = smtProductList::where('productId', $productItem['productId'])->first();
-                        if ($res) {
-                            smtProductList::where('productId', $productItem['productId'])->update($product);
-                        } else {
-                            smtProductList::create($product);
-                        }
-    
-                        $productDetail['productId'] = $productItem['productId'];
-                        $productDetail['aeopAeProductPropertys'] = serialize($productInfo['aeopAeProductPropertys']);
-                        $productDetail['imageURLs'] = $productInfo['imageURLs'];
-                        $productDetail['detail'] = $productInfo['detail'];
-                        $productDetail['productUnit'] = $productInfo['productUnit'];
-                        $productDetail['isImageDynamic'] = $productInfo['isImageDynamic'] ? 1 : 0;
-                        $productDetail['lotNum'] = $productInfo['lotNum'];
-                        $productDetail['bulkOrder'] = array_key_exists('bulkOrder', $productInfo) ? $productInfo['bulkOrder'] : '';
-                        $productDetail['packageType'] = $productInfo['packageType'];
-                        $productDetail['isPackSell'] = $productInfo['isPackSell'] ? 1 : 0;
-                        $productDetail['promiseTemplateId'] = $productInfo['promiseTemplateId'];
-                        $productDetail['freightTemplateId'] = $productInfo['freightTemplateId'];
-                        $productDetail['src'] =  array_key_exists('src', $productInfo) ? $productInfo['src'] : '';
-                        $productDetail['bulkDiscount'] = array_key_exists('bulkDiscount', $productInfo) ? $productInfo['bulkDiscount'] : '';
-                        $detail = smtProductDetail::where('productId', $productItem['productId'])->first();
-                        if ($detail) {
-                            smtProductDetail::where('productId', $productItem['productId'])->update($productDetail);
-                        } else {
-                            smtProductDetail::create($productDetail);
-                        }
-    
-                        foreach ($productInfo['aeopAeProductSKUs'] as $skuItem) {
-                            $skuInfo = smtProductSku::where(['smtSkuCode' => $skuItem['skuCode'], 'productId' => $productItem['productId']])->first();
-                            $skuData = array();
-                            $skuData['aeopSKUProperty'] = serialize($skuItem['aeopSKUProperty']);
-                            $skuCode = $this->getSkuCode($skuItem['skuCode']);
-                            $skuData['skuCode'] = $skuCode;
-                            $skuData['skuMark'] = $productItem['productId'].':'.$skuCode;
-                            $skuData['smtSkuCode'] = $skuItem['skuCode'];
-                            $skuData['skuPrice'] = $skuItem['skuPrice'];
-                            $skuData['ipmSkuStock'] = $skuItem['ipmSkuStock'];
-                            $skuData['productId'] = $productItem['productId'];
-                            if ($skuInfo) {
-                                smtProductSku::where(['smtSkuCode' => $skuItem['skuCode'], 'productId' => $productItem['productId']])->update($skuData);
-                            } else {
-                                smtProductSku::create($skuData);
-                            }
-                        }
-                    }
-                    $currentPage++;
-                }else{
-                    $flag = false;
-                }
-            }
-           
-        }
-        $end = microtime(true);
-        echo '耗时' . round($end - $start, 3) . '秒';
-        /*if ($account) {
+        $account = AccountModel::find($this->argument('accountID'));
+        if ($account) {
             $channel = Channel::driver($account->channel->driver, $account->api_config);
            
             $flag = true;
@@ -221,7 +130,7 @@ class GetAliexpressProduct extends Command
             $end = microtime(true);
             echo '耗时' . round($end - $start, 3) . '秒';
 
-        }*/
+        }
     }
 
     /**

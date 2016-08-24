fcd6ee21a6644bb45676bbb500a2e9c28bee3f1f
nba694929659 <694929659@qq.com>
2016-08-24 18:11:00 +0800

    no message

--- database/migrations/2016_08_13_150243_create_ebay_cases_lists_table.php
+++ database/migrations/2016_08_13_150243_create_ebay_cases_lists_table.php
@@ -27,6 +27,8 @@ class CreateEbayCasesListsTable extends Migration
             $table->string('respon_date')->commit('响应日期')->nullable();
             $table->string('creation_date')->commit('创建日期')->nullable();
             $table->string('last_modify_date')->commit('修改日期')->nullable();
+            $table->integer('assign_id')->commit('处理人')->nullable();
+            $table->integer('account_id')->commit('渠道账号')->nullable();
 
             $table->string('global_id')->nullable();
             $table->string('open_reason')->nullable();

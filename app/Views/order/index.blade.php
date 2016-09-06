924182d1b8da3f3f1568f5130eaf30ccfc02bb51
jiangdi <694929659@qq.com>
2016-09-06 19:39:18 +0800

    no message

--- app/Views/order/index.blade.php
+++ app/Views/order/index.blade.php
@@ -193,6 +193,9 @@
                             <?php break ?>
                         @endif
                     @endforeach
+                    <button class="btn btn-primary btn-xs" data-toggle="modal" data-target="#send_ebay_message_{{ $order->id }}" title="Send ebay Message">
+                        <span class="glyphicon glyphicon-envelope"></span> Send ebay Message
+                    </button>
                     <button class="btn btn-primary btn-xs"
                             data-toggle="modal"
                             data-target="#remark{{ $order->id }}"
@@ -543,6 +546,48 @@
                         @else
                         @endif
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" id="send_ebay_message_{{ $order->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <form action="{{ route('withdrawUpdate', ['id' => $order->id])}}" method="POST">
+                        {!! csrf_field() !!}
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <h4 class="modal-title" id="myModalLabel">撤单</h4>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="form-group col-lg-6">
+                                    <label for="withdraw" class='control-label'>撤单原因</label>
+                                    <small class="text-danger glyphicon glyphicon-asterisk"></small>
+                                    <select class="form-control" name="withdraw" id="withdraw">
+                                        <option value="NULL">==选择原因==</option>
+                                        @foreach(config('order.withdraw') as $withdraw_key => $withdraw)
+                                            <option value="{{ $withdraw_key }}" {{ old('withdraw') == $withdraw_key ? 'selected' : '' }}>
+                                                {{ $withdraw }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="form-group col-lg-6">
+                                    <label for="withdraw_reason" class='control-label'>原因</label>
+                                    <textarea class="form-control" rows="3" name='withdraw_reason' id="withdraw_reason">{{ old('withdraw_reason') }}</textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                            <button type="submit" class="btn btn-primary">提交</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>

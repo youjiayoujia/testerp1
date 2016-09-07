5283f08b49d147a4835a91b66e879c2b0f2a75d3
mcss <178069409@qq.com>
2016-09-07 14:32:30 +0800

    xiugai

--- database/migrations/2016_09_07_143055_delete_package_status_column.php
+++ database/migrations/2016_09_07_143055_delete_package_status_column.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterPackage extends Migration
+class DeletePackageStatusColumn extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,6 @@ class AlterPackage extends Migration
     {
         Schema::table('packages', function (Blueprint $table) {
             $table->dropColumn('status');
-            
         });
     }
 

cbd27f50c49a5834303104ff507eb0b05bc269ef
卞海伟 <bianhaiwei@bianhaiweideMacBook-Pro.local>
2016-09-22 23:34:44 +0800

    no message

--- app/Views/logistics/template/tpl/printDiouPY_ldb_tlp.blade.php
+++ app/Views/logistics/template/tpl/printDiouPY_ldb_tlp.blade.php
@@ -25,45 +25,47 @@
     <table cellspacing=0 cellpadding=0>
         <tr style="display:block;">
             <td style="border:1px solid black;width:48mm;height:30mm" valign="top">
-                <img src="'.site_url('attachments').'/images/Bpostlogo1.jpg" style="width:46mm;height:14mm"/>
-
+                <img src="{{ asset('picture/Bpostlogo1.jpg') }}" style="width:46mm;height:14mm"/>
                 <p style="padding:2px;font-size:12px;">
                     If undelivered please return to<br/>ECDC LOGISTICS<br/>Rue de Maastricht 106 4600,Vise Belgium
                 </p>
             </td>
             <td style="border:1px solid black;width:40mm;border-left:none;padding:1mm 3mm;text-align:center;" valign="top">
-                <img src="'.site_url('attachments').'/images/Bpostlogo2.jpg" style="width:37mm;height:14mm"/>
-
+                <img src="{{ asset('picture/Bpostlogo2.jpg') }}" style="width:37mm;height:14mm"/>
                 <p style="font-weight: bold;font-size:18px;">PB-PP BPI-9572</p>
-
                 <div style="text-align:right;height:10mm;line-height: 50px;">BELGIE(N)-BELGIQUE</div>
             </td>
         </tr>
         <tr>
             <td colspan="2" style="padding:1mm;font-size:15px;line-height: 19px;">
-                TO:'.$allParamArr['ordersInfo']['buyer_name'].'<br/>'
-                . $allParamArr['ordersInfo']['buyer_address_1'] .' <br/>'. $allParamArr['ordersInfo']['buyer_address_2']
-                .'<br/>
-                '. $allParamArr['ordersInfo']['buyer_city'] .' '. $allParamArr['ordersInfo']['buyer_state'] .'<br/>
-                Zip Code:'.$allParamArr['ordersInfo']['buyer_zip'].'<br/>
-                Country:'.$allParamArr['buyerCountry'].'('.$allParamArr['country_code'].')<br/>
-                Phone:'.$allParamArr['ordersInfo']['buyer_phone'].'<br>
-                Weight:'.sprintf("%01.2f",$allParamArr['productsInfo']['total_weight']).'<br/>
-                Ref:'.$allParamArr['ordersInfo']['erp_orders_id'].'
+                TO:{{ $model->shipping_firstname . ' ' . $model->shipping_lastname }}<br/>
+                {{ $model->shipping_address }}<br/>
+                {{ $model->shipping_address1 }}<br/>
+                {{ $model->shipping_city . ' ' . $model->shipping_state }}<br/>
+                Zip Code:{{ $model->shipping_zipcode }}<br/>
+                Country:{{ $model->country ? $model->country->name : '' . '(' }}{{ $model->country ? $model->country->code : '' . ')' }}<br/>
+                Phone:{{ $model->shipping_phone }}<br>
+                Weight:
+                @if($model->items)
+                    @foreach($model->items as $item)
+                        {{ sprintf("%01.2f", $item->quantity * $item->item->weight) }}
+                    @endforeach
+                @endif
+                <br/>
+                Ref:{{ $model->order_id }}
             </td>
         </tr>
         <tr style="margin-top:10px;display:block;width:95mm">
             <td colspan="2" style="border:1px dashed black;padding:1mm;text-align:center;width:95mm">
-                <img src="'.site_url('default/third_party').'/chanage_code/barcode/html/image.php?code=code128&o=2&t=50&r=1&text='.$allParamArr['ordersInfo']['orders_shipping_code'].'&f1=-1&f2=8&a1=&a2=B&a3="/>
-
-                <p>'.$allParamArr['ordersInfo']['orders_shipping_code'].'</p>
+                <img src="{{ route('barcodeGen', ['content' => $model->tracking_no]) }}">
+                <p>{{ $model->tracking_no }}</p>
             </td>
         </tr>
         <tr style="display:block;width:95mm">
             <td colspan="2" style="width:95mm;">
                 <div style="border:1px solid black;height:40px;text-align:center;padding-top:15px;margin-top:10px;">
-                    <div style="margin:0 auto;border:1px solid black;padding:2mm;text-align:center;width:20px;height;20px;font-size:20px;font-weight: bold;">
-                        '.$allParamArr['bilishiArea'].'
+                    <div style="margin:0 auto;border:1px solid black;padding:2mm;text-align:center;width:20px;height:20px;font-size:20px;font-weight: bold;">
+                        {{ $model->country ? ($model->country->area != null ? $model->country->area : 0) : 0 }}
                     </div>
                 </div>
             </td>

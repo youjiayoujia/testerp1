71f057300441a4ab0f532cfd6b344c5cdb95628e
卞海伟 <bianhaiwei@bianhaiweideMacBook-Pro.local>
2016-09-22 16:18:16 +0800

    no message

--- app/Views/logistics/template/tpl/printNewSFHL_ldb_tlp.blade.php
+++ app/Views/logistics/template/tpl/printNewSFHL_ldb_tlp.blade.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <title>EUB-100×100热敏标签</title>
+        <title>新顺丰荷兰面单</title>
     </head>
     <body>
     <style>

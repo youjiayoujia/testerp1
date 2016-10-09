2965f0a5c82e7999b7f1be06d867421f387cbdb6
jiangdi <694929659@qq.com>
2016-10-09 16:46:53 +0800

    no message

--- app/Models/Channel/AccountModel.php
+++ app/Models/Channel/AccountModel.php
@@ -52,7 +52,7 @@ class AccountModel extends BaseModel
     }
     public function catalogChannel()
     {
-        return $this->belongsTo('App\Models\channel\CatalogRatesModel', 'catalog_rates_channel_id', 'id');
+        return $this->belongsTo('App\Models\Channel\CatalogRatesModel', 'catalog_rates_channel_id', 'id');
     }
 
     public function country()

a49593a95fec760ea4b05dfc75ed719cc7bad526
mcss <178069409@qq.com>
2016-10-12 17:00:48 +0800

    no message

--- app/Models/Product/ImageModel.php
+++ app/Models/Product/ImageModel.php
@@ -96,23 +96,28 @@ class ImageModel extends BaseModel
      */
     public function imageCreate($data, $files = null)
     {
-        var_dump($data);exit;
+        $buf = [];
         $data['path'] = config('product.image.uploadPath') . '/' . $data['spu_id'] . '/' . $data['product_id'] . '/' . $data['is_link'] . '/';
         $disk = Storage::disk('product');
         switch ($data['uploadType']) {
             case 'image':
                 foreach ($files as $key => $file) {
                     if ($this->valid($file->getClientOriginalName())) {
-                        $data['name'] = time() . $key . '.' . $file->getClientOriginalExtension();
+                        $buf1 = explode(' ', microtime());
+                        $tmp = (float)$buf1[1] + (float)$buf1[0];
+                        $data['name'] = $tmp . '.' . $file->getClientOriginalExtension();
+                        $buf['filename'] = $data['name'];
+                        $buf['tags'] = $this->tagEng($data['tag']);
+                        $times = explode(' ', microtime());
+                        $buf['lastUpdated']['sec'] = $times['1'];
+                        $buf['lastUpdated']['usec'] = substr($times['0'],2,6);
                         Storage::disk('product')->put($data['path'].$data['name'],file_get_contents($file->getRealPath()));
                         $imageModel = $this->create($data);
                         $arr[] = $data['is_link'];
-
                         $imageModel->labels()->attach($arr);
                         if($data['tag']!=''){
                             $imageModel->labels()->attach($data['tag']);
                         }
-                        
                     }
                 }
                 break;
@@ -131,8 +136,43 @@ class ImageModel extends BaseModel
                 }
                 break;
         }
+        
+        return json_encode($buf);
+    }
 
-        return 1;
+    public function tagEng($arr)
+    {
+        $buf = [];
+        foreach($arr as $key => $value) {
+            switch($value) {
+                case 1:
+                    $buf[] = 'photo';
+                    break;
+                case 2:
+                    $buf[] = 'link';
+                    break;
+                case 3:
+                    $buf[] = 'normal';
+                    break;
+                case 4:
+                    $buf[] = 'shape';
+                    break;
+                case 5:
+                    $buf[] = 'front';
+                    break;
+                case 6:
+                    $buf[] = 'logo';
+                    break;
+                case 7:
+                    $buf[] = 'color';
+                    break;
+                case 8:
+                    $buf[] = 'size';
+                    break;
+            }
+        }
+
+        return $buf;
     }
 
     /**

647c9b382554107fcca590f5b0525f8828cdf3f0
jiangdi <694929659@qq.com>
2016-10-20 19:33:34 +0800

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -59,6 +59,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\AutoGetMessageAliexpress::class,
         \App\Console\Commands\AutoGetWishMessage::class,
         \App\Console\Commands\inputCrmTemplate::class, //导入CRM分类和模板
+        \App\Console\Commands\inputPaypalList::class, //导入CRM分类和模板
 
     ];
 

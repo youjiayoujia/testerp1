ad0a15e0c48d1a027efad7afd53435544f7acfcf
Auser <Auser>
2016-10-26 22:27:23 +0800

    1

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -88,6 +88,7 @@ class ItemController extends Controller
         if (!$model) {
             return redirect($this->mainIndex)->with('alert', $this->alert('danger', $this->mainTitle . '不存在.'));
         }
+
         request()->flash();
         $this->validate(request(), $this->model->rules('update', $id));
         $model->updateItem($data);
@@ -162,21 +163,21 @@ class ItemController extends Controller
         $skuModel = $this->model->where('sku',$data['sku'])->get()->first();
         if($data['type']='edit'){
             $skuModel->update($data);
-            foreach($data['carriage_limit_arr'] as $logistics_limits_id){
+            foreach(unserialize($data['carriage_limit_arr']) as $logistics_limits_id){
                 $brr[] = $logistics_limits_id;         
             }
             $skuModel->product->logisticsLimit()->sync($brr);
-            foreach($data['package_limit_arr'] as $wrap_limits_id){
+            foreach(unserialize($data['package_limit_arr']) as $wrap_limits_id){
                 $arr[] = $wrap_limits_id;         
             }
             $skuModel->product->wrapLimit()->sync($arr);
         }else{
             $skuModel->create($data);
-            foreach($data['carriage_limit_arr'] as $logistics_limits_id){
+            foreach(unserialize($data['carriage_limit_arr']) as $logistics_limits_id){
                 $brr[] = $logistics_limits_id;         
             }
             $skuModel->product->logisticsLimit()->attach($brr);
-            foreach($data['package_limit_arr'] as $wrap_limits_id){
+            foreach(unserialize($data['package_limit_arr']) as $wrap_limits_id){
                 $arr[] = $wrap_limits_id;         
             }
             $skuModel->product->wrapLimit()->attach($arr);

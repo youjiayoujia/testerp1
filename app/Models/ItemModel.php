ec21102a8a0f2f4f123f344422708f1e3c9a4ab3
Auser <Auser>
2016-10-27 17:31:09 +0800

    1

--- app/Models/ItemModel.php
+++ app/Models/ItemModel.php
@@ -824,7 +824,7 @@ class ItemModel extends BaseModel
     public function updateOldData()
     {
         set_time_limit(0);
-        $model = $this->where('sku','CA1205W')->get();
+        $model = $this->all();
         foreach ($model as $key => $itemModel) {
             $erp_products_data = DB::select('select pack_method,products_with_battery,products_with_adapter,products_with_fluid,products_with_powder 
                     from erp_products_data where products_sku =  "'.$itemModel->sku.'" ');

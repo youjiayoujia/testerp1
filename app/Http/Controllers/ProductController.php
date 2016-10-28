ced05fc13e380ac4d114150267fb3731ec47e36d
jiangdi <694929659@qq.com>
2016-10-28 11:35:46 +0800

    no message

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -138,6 +138,7 @@ class ProductController extends Controller
             'logisticsLimit' => $this->logisticsLimit->all(),
             'wrapLimit_arr' => $wrapLimit_arr,
             'logisticsLimit_arr' => $logisticsLimit_arr,
+            'all_second_suppliers' =>SupplierModel::where('examine_status','currentData')->get(),
         ];
         return view($this->viewPath . 'edit', $response);
     }
--- app/Models/Product/SupplierModel.php
+++ app/Models/Product/SupplierModel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\product;
+namespace App\Models\Product;
 
 use App\Base\BaseModel;
 use Tool;

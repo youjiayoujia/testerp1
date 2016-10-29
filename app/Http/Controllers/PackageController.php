15bd27fd3f80583660a51badec4f76be0d8ce501
zhangmengying <mengying.zhang110@gmail.com>
2016-10-29 09:37:04 +0800

    no message

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -98,7 +98,7 @@ class PackageController extends Controller
         request()->flash();
         $response = [
             'metas' => $this->metas(__FUNCTION__),
-            'data' => $this->autoList(!empty($buf) ? $buf : $this->model),
+            'data' => $this->autoList(!empty($buf) ? $buf : $this->model->with('order')->with('warehouse')->with('logistics')->with('items')->with('channel')->with('picklist')),
             'mixedSearchFields' => $this->model->mixed_search,
             'logisticses' => LogisticsModel::all(),
         ];

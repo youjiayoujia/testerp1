f94e10a1653efd28f7b80aec81d1d8478bd01ad4
guoou <916091060@qq.com>
2016-11-01 16:40:47 +0800

    modify

--- app/Modules/Logistics/Adapter/SmtAdapter.php
+++ app/Modules/Logistics/Adapter/SmtAdapter.php
@@ -192,12 +192,12 @@ class SmtAdapter extends BasicAdapter
                     foreach ($res['result'] as $row){
                         //分仓，估计要结合 物流分类+状态 来进行判断获取国际运单号
                         if ($row['internationalLogisticsType'] == $warehouseCarrierService && $row['onlineLogisticsId'] == $onlineLogisticsId) { //渠道和物流内单号对应上了
-                            return array('code' => 'success', 'result' => $row['internationallogisticsId']);//国际运单号;
+                            return array('code' => 'success', 'result' => $row['internationallogisticsId'],'result_other' => $onlineLogisticsId);//国际运单号;
                         }
                     }
                 }else{
-                            return array('code' => 'again', 'result' => '', 'result_other' => $onlineLogisticsId);//国际运单号;
-                        }
+                    return array('code' => 'again', 'result' => '', 'result_other' => $onlineLogisticsId);//国际运单号;
+                }
             }
             
         }

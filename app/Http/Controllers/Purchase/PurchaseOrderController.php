a12909b2a2e61c2b8f2a8cf35bc80fd90bdaec71
Auser <Auser>
2016-11-03 19:52:28 +0800

    1

--- app/Http/Controllers/Purchase/PurchaseOrderController.php
+++ app/Http/Controllers/Purchase/PurchaseOrderController.php
@@ -94,7 +94,7 @@ class PurchaseOrderController extends Controller
     public function store()
     {
         request()->flash();
-        //$this->validate(request(), $this->model->rules('create'));
+        $this->validate(request(), $this->model->rules('create'));
         $this->model->createPurchaseOrder(request()->all());
 
         return redirect($this->mainIndex)->with('alert', $this->alert('success', '添加成功.'));

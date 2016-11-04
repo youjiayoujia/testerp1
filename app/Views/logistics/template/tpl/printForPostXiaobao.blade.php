31b40277f5e556552d0b6245496d56ab2b110319
hejiancheng <hejiancheng@moonarstore.com>
2016-11-04 13:55:49 +0800

    update

--- app/Views/logistics/template/tpl/printForPostXiaobao.blade.php
+++ app/Views/logistics/template/tpl/printForPostXiaobao.blade.php
@@ -163,7 +163,7 @@
 
 
                     {{ $model->declared_cn ? $model->declared_cn : ''}}<br>*
-                    {{ $model->declared_en ? $model->declared_en : ''}}*{{ $model->items()->sum('quantity') }}
+                    {{ $model->declared_en ? $model->declared_en : ''}}*{{ $model->items ? $model->items->sum('quantity') : 0 }}
                 </td>
                 <td style="border-bottom: 1px solid black;border-right:1px solid black;" align="center">
                     {{ $model->total_weight }}

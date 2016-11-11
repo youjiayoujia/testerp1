d5d2c4f899873fddf91af53f6e1eb270188e2e47
jiangdi <694929659@qq.com>
2016-11-11 19:34:15 +0800

    no message

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -154,6 +154,10 @@ Route::group(['middleware' => 'roleCheck'], function () {
         ['as' => 'product.ajaxReturnLogistics', 'uses' => 'ProductController@ajaxReturnLogistics']);
     Route::any('ajaxReutrnCatalogs',
         ['uses' => 'ProductController@ajaxReutrnCatalogs', 'as' => 'ajaxReutrnCatalogs']);
+    Route::any('submitItemEdit',
+        ['uses' => 'ProductController@submitItemEdit', 'as' => 'product.submitItemEdit']);
+    Route::any('edititemattribute/{id}', ['uses' => 'ProductController@editItemAttribute']);
+
     //通关报关
     Route::post('customsClearance/exportProduct',
         ['uses' => 'CustomsClearanceController@exportProduct', 'as' => 'customsClearance.exportProduct']);

03da383f5c7a397cadaff389707a0e29fb0a81ab
youj <2016696078@qq.com>
2016-11-15 16:52:54 +0800

    2

--- app/Http/Controllers/Purchase/RequireController.php
+++ app/Http/Controllers/Purchase/RequireController.php
@@ -17,6 +17,7 @@ use App\Models\Purchase\PurchaseOrderModel;
 use App\Models\Purchase\PurchaseRequireModel;
 use App\Models\Product\SupplierModel;
 use App\Models\StockModel;
+use App\Models\UserModel;
 use App\Models\PackageModel;
 use App\Models\Package\ItemModel;
 use App\Models\ItemModel as ProductItemModel;
@@ -83,12 +84,15 @@ class RequireController extends Controller
 			$data['type']=0;
 			//$data['warehouse_id']=$v->warehouse_id ? $v->warehouse_id : 0;
 			//测试专用仓库
-			if($v['warehouse_id']==1){
+			/*if($v['warehouse_id']==1){
                 $data['warehouse_id']=3;
             }
             if($v['warehouse_id']==2){
                 $data['warehouse_id']=4;
-            }
+            }*/
+
+            //根据采购人绑定仓库
+            $data['warehouse_id'] = UserModel::find($v->purchase_adminer)->warehouse_id;
             
 			$data['sku']=$v->sku;
 			$data['item_id']=$v->id;

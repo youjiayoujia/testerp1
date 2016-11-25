72553dee14ede257a903460ab144a72cc16dc2b8
卞海伟 <bianhaiwei@bianhaiweideMacBook-Pro.local>
2016-11-25 16:42:05 +0800

    no message

--- app/Views/countries/change/edit.blade.php
+++ app/Views/countries/change/edit.blade.php
@@ -6,7 +6,13 @@
         <div class="form-group col-lg-2">
             <label for="country_from" class='control-label'>来源国家</label>
             <small class="text-danger glyphicon glyphicon-asterisk"></small>
-            <input class="form-control" placeholder="来源国家" name='country_from' value="{{ old('country_from') ? old('country_from') : $model->country_from }}">
+            <select name="country_from" class="form-control country_from" id="country_from">
+                @foreach($countries as $country)
+                    <option value="{{$country->code}}" {{$country->code == $model->country_from ? 'selected' : ''}}>
+                        {{$country->code . ' ' . $country->cn_name}}
+                    </option>
+                @endforeach
+            </select>
         </div>
         <div class="form-group col-lg-2">
             <label for="country_to" class='control-label'>目标国家</label>
@@ -14,4 +20,11 @@
             <input class="form-control" placeholder="目标国家" name='country_to' value="{{ old('country_to') ? old('country_to') : $model->country_to }}">
         </div>
     </div>
+@stop
+@section('pageJs')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.country_from').select2();
+        });
+    </script>
 @stop

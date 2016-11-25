6faa1243345ae61b855ffd3e477b86bb4fc5c504
guoou <916091060@qq.com>
2016-11-25 15:39:11 +0800

    modify

--- app/Modules/Logistics/Adapter/SzPostXBAdapter.php
+++ app/Modules/Logistics/Adapter/SzPostXBAdapter.php
@@ -284,7 +284,7 @@ class SzPostXBAdapter extends BasicAdapter
         $curl = curl_init (); // 启动一个CURL会话
         curl_setopt ( $curl, CURLOPT_URL, $url ); // 要访问的地址
         curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, 0 ); // 对认证证书来源的检查
-        curl_setopt ( $curl, CURLOPT_SSL_VERIFYHOST, 1 ); // 从证书中检查SSL加密算法是否存在
+        curl_setopt ( $curl, CURLOPT_SSL_VERIFYHOST, 2 ); // 从证书中检查SSL加密算法是否存在
         //curl_setopt ( $curl, CURLOPT_USERAGENT, $_SERVER ['HTTP_USER_AGENT'] ); // 模拟用户使用的浏览器
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt ( $curl, CURLOPT_FOLLOWLOCATION, 1 ); // 使用自动跳转

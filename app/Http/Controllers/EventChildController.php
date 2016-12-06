ffcf43de9d2e34bcbf34796cb17a6e8be9c8a376
Vincent <nyewon@gmail.com>
2016-12-06 10:15:50 +0800

    update

--- app/Http/Controllers/EventChildController.php
+++ app/Http/Controllers/EventChildController.php
@@ -27,29 +27,29 @@ class EventChildController extends Controller
     public function stdClass_to_array($object)
     {
         $arr = (array)$object;
-        foreach($arr as $key => $value) {
-            if(is_object($value)) {
-               $arr[$key] = $this->stdClass_to_array($value); 
-               continue;
-           }
-           if(is_array($value)) {
-                foreach($value as $k => $v) {
-                    if(is_object($v)) {
+        foreach ($arr as $key => $value) {
+            if (is_object($value)) {
+                $arr[$key] = $this->stdClass_to_array($value);
+                continue;
+            }
+            if (is_array($value)) {
+                foreach ($value as $k => $v) {
+                    if (is_object($v)) {
                         $value[$k] = $this->stdClass_to_array($v);
                     }
                 }
-           }
-           $arr[$key] = $value;
+            }
+            $arr[$key] = $value;
         }
 
         return $arr;
     }
 
     /**
-     *  返回对应的操作日志,ajax请求 
+     *  返回对应的操作日志,ajax请求
      *
-     *  @param none
-     *  @return html
+     * @param none
+     * @return html
      *
      */
     public function getInfo()
@@ -57,47 +57,47 @@ class EventChildController extends Controller
         $table = request('table');
         $id = request('id');
         $category = CategoryModel::where('model_name', $table)->first();
-        if(!$category) {
+        if (!$category) {
             return false;
         }
         $models = $category->child()->where('type_id', $id)->get()->sortByDesc('when');
         $html = '';
-        foreach($models as $key => $model) {
+        foreach ($models as $key => $model) {
             $to = json_decode($model->to_arr);
-            if($to) {
+            if ($to) {
                 $to = $this->stdClass_to_array($to);
             }
             $from = json_decode($model->from_arr);
-            if($from) {
+            if ($from) {
                 $from = $this->stdClass_to_array($from);
             }
-            if(!$from) {
+            if (!$from) {
                 $html .= "<div class='panel panel-default'>
-                        <div class='panel-heading'>备注:".$model->what. '&nbsp;&nbsp;&nbsp;&nbsp;操作时间:' . $model->when . "&nbsp;&nbsp;&nbsp;&nbsp;操作人:". $model->who . "
-                    <a class='btn btn-xs btn-primary' role='button' data-toggle='collapse' href='#collapseExample".$key."' aria-expanded='false' aria-controls='collapseExample'>数据详情</a>
+                        <div class='panel-heading'>备注:" . $model->what . '<br/>操作时间:' . $model->when . "&nbsp;&nbsp;&nbsp;&nbsp;操作人:" . $model->who . "
+                    <a class='btn btn-xs btn-primary' role='button' data-toggle='collapse' href='#collapseExample" . $key . "' aria-expanded='false' aria-controls='collapseExample'>数据详情</a>
                         </div>
-                        <div class='panel-body collapse' id='collapseExample".$key."'><div class='col-lg-12'>";
-                foreach($to as $key => $value) {
-                    $html .= "<div class='row'>to['".$key."']<span class='glyphicon glyphicon-arrow-right'></span>";
-                    if(is_array($value)) {
-                        if(count($value) == count($value,1)) {
-                            $html .="<div class='col-lg-12'><div class='row'>";
-                            foreach($value as $k => $v) {
-                                if(!is_array($v)) {
-                                    $html .= "to['".$key."']['".$k."']<span class='glyphicon glyphicon-arrow-right'></span>".$v."&nbsp;&nbsp;&nbsp;&nbsp;";
+                        <div class='panel-body collapse' id='collapseExample" . $key . "'><div class='col-lg-12'>";
+                foreach ($to as $key => $value) {
+                    $html .= "<div class='row'>to['" . $key . "']<span class='glyphicon glyphicon-arrow-right'></span>";
+                    if (is_array($value)) {
+                        if (count($value) == count($value, 1)) {
+                            $html .= "<div class='col-lg-12'><div class='row'>";
+                            foreach ($value as $k => $v) {
+                                if (!is_array($v)) {
+                                    $html .= "to['" . $key . "']['" . $k . "']<span class='glyphicon glyphicon-arrow-right'></span>" . $v . "&nbsp;&nbsp;&nbsp;&nbsp;";
                                 }
                             }
                             $html .= '</div></div>';
                         } else {
-                            foreach($value as $k => $v) {
-                                if(is_array($v)) {
-                                    $html .="<div class='col-lg-12'><div class='row'>";
-                                    foreach($v as $k1 => $v1) {
-                                        if(!is_array($v1)) {
-                                            $html .= "to['".$key."']['".$k."']['".$k1."']<span class='glyphicon glyphicon-arrow-right'></span>".$v1."&nbsp;&nbsp;&nbsp;&nbsp;";
+                            foreach ($value as $k => $v) {
+                                if (is_array($v)) {
+                                    $html .= "<div class='col-lg-12'><div class='row'>";
+                                    foreach ($v as $k1 => $v1) {
+                                        if (!is_array($v1)) {
+                                            $html .= "to['" . $key . "']['" . $k . "']['" . $k1 . "']<span class='glyphicon glyphicon-arrow-right'></span>" . $v1 . "&nbsp;&nbsp;&nbsp;&nbsp;";
                                         }
                                     }
-                                    $html .="</div></div>";
+                                    $html .= "</div></div>";
                                 }
                             }
                         }
@@ -105,37 +105,37 @@ class EventChildController extends Controller
                         $html .= $value;
                     }
                     $html .= "</div>";
-                }  
+                }
                 $html .= "</div></div></div>";
-                continue;     
+                continue;
             }
             $html .= "<div class='panel panel-default'>
-                        <div class='panel-heading'>备注:".$model->what. '&nbsp;&nbsp;&nbsp;&nbsp;操作时间:' . $model->when . "&nbsp;&nbsp;&nbsp;&nbsp;操作人:". $model->who . "<a class='btn btn-xs btn-primary' role='button' data-toggle='collapse' href='#collapseExample".$key."' aria-expanded='false' aria-controls='collapseExample'>数据详情</a></div>
-                        <div class='panel-body collapse'  id='collapseExample".$key."'><div class='col-lg-12'>";
+                        <div class='panel-heading'>备注:" . $model->what . '<br/>操作时间:' . $model->when . "&nbsp;&nbsp;&nbsp;&nbsp;操作人:" . $model->who . "<a class='btn btn-xs btn-primary' role='button' data-toggle='collapse' href='#collapseExample" . $key . "' aria-expanded='false' aria-controls='collapseExample'>数据详情</a></div>
+                        <div class='panel-body'  id='collapseExample" . $key . "'><div class='col-lg-12'>";
             $flag = 1;
-            $this->calcTwoArr($from,$to);
-            foreach($from as $key => $value) {
-                $html .= "<div class='row'>from['".$key."']<span class='glyphicon glyphicon-arrow-right'></span>";
-                if(is_array($value)) {
-                    $html .="<div class='col-lg-12'><div class='row'>";
-                    if(count($value) == count($value,1)) {
-                        $html .="<div class='row'>";
-                        foreach($value as $k => $v) {
-                            if(!is_array($v)) {
-                               $html .= "to['".$key."']['".$k."']<span class='glyphicon glyphicon-arrow-right'></span>".$v."&nbsp;&nbsp;&nbsp;&nbsp;"; 
-                           }
+            $this->calcTwoArr($from, $to);
+            foreach ($from as $key => $value) {
+                $html .= "<div class='row'>from['" . $key . "']<span class='glyphicon glyphicon-arrow-right'></span>";
+                if (is_array($value)) {
+                    $html .= "<div class='col-lg-12'><div class='row'>";
+                    if (count($value) == count($value, 1)) {
+                        $html .= "<div class='row'>";
+                        foreach ($value as $k => $v) {
+                            if (!is_array($v)) {
+                                $html .= "to['" . $key . "']['" . $k . "']<span class='glyphicon glyphicon-arrow-right'></span>" . $v . "&nbsp;&nbsp;&nbsp;&nbsp;";
+                            }
                         }
-                        $html .="</div></div>";
+                        $html .= "</div></div>";
                     } else {
-                        foreach($value as $k => $v) {
-                            if(is_array($v)) {
-                                $html .="<div class='col-lg-12'><div class='row'>";
-                                foreach($v as $k1 => $v1) {
-                                    if(!is_array($v1)) {
-                                       $html .= "to['".$key."']['".$k."']['".$k1."']<span class='glyphicon glyphicon-arrow-right'></span>".$v1."&nbsp;&nbsp;&nbsp;&nbsp;"; 
-                                   }
+                        foreach ($value as $k => $v) {
+                            if (is_array($v)) {
+                                $html .= "<div class='col-lg-12'><div class='row'>";
+                                foreach ($v as $k1 => $v1) {
+                                    if (!is_array($v1)) {
+                                        $html .= "to['" . $key . "']['" . $k . "']['" . $k1 . "']<span class='glyphicon glyphicon-arrow-right'></span>" . $v1 . "&nbsp;&nbsp;&nbsp;&nbsp;";
+                                    }
                                 }
-                                $html .="</div></div>";
+                                $html .= "</div></div>";
                             }
                         }
                     }
@@ -145,27 +145,27 @@ class EventChildController extends Controller
                 $html .= "</div>";
             }
             $html .= "<hr/>";
-            foreach($to as $key => $value) {
-                $html .= "<div class='row'>to['".$key."']<span class='glyphicon glyphicon-arrow-right'></span>";
-                if(is_array($value)) {
-                    if(count($value) == count($value,1)) {
-                        $html .="<div class='col-lg-12'><div class='row'>";
-                        foreach($value as $k => $v) {
-                            if(!is_array($v)) {
-                              $html .= "to['".$key."']['".$k."']<span class='glyphicon glyphicon-arrow-right'></span>".$v."&nbsp;&nbsp;&nbsp;&nbsp;";  
-                          }
+            foreach ($to as $key => $value) {
+                $html .= "<div class='row'>to['" . $key . "']<span class='glyphicon glyphicon-arrow-right'></span>";
+                if (is_array($value)) {
+                    if (count($value) == count($value, 1)) {
+                        $html .= "<div class='col-lg-12'><div class='row'>";
+                        foreach ($value as $k => $v) {
+                            if (!is_array($v)) {
+                                $html .= "to['" . $key . "']['" . $k . "']<span class='glyphicon glyphicon-arrow-right'></span>" . $v . "&nbsp;&nbsp;&nbsp;&nbsp;";
+                            }
                         }
-                        $html .="</div></div>";
+                        $html .= "</div></div>";
                     } else {
-                        foreach($value as $k => $v) {
-                            if(is_array($v)) {
-                                $html .="<div class='col-lg-12'><div class='row'>";
-                                foreach($v as $k1 => $v1) {
-                                    if(!is_array($v1)) {
-                                       $html .= "to['".$key."']['".$k."']['".$k1."']<span class='glyphicon glyphicon-arrow-right'></span>".$v1."&nbsp;&nbsp;&nbsp;&nbsp;"; 
+                        foreach ($value as $k => $v) {
+                            if (is_array($v)) {
+                                $html .= "<div class='col-lg-12'><div class='row'>";
+                                foreach ($v as $k1 => $v1) {
+                                    if (!is_array($v1)) {
+                                        $html .= "to['" . $key . "']['" . $k . "']['" . $k1 . "']<span class='glyphicon glyphicon-arrow-right'></span>" . $v1 . "&nbsp;&nbsp;&nbsp;&nbsp;";
                                     }
                                 }
-                                $html .="</div></div>";
+                                $html .= "</div></div>";
                             }
                         }
                     }
@@ -174,7 +174,7 @@ class EventChildController extends Controller
                 }
                 $html .= "</div>";
             }
-            $html.= '</div></div></div>';
+            $html .= '</div></div></div>';
         }
         return $html;
     }

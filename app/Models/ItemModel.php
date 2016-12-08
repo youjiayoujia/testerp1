82153725e3b7688612b0acc7e2ff73482d44bf62
youj <2016696078@qq.com>
2016-12-08 17:21:06 +0800

    1

--- app/Models/ItemModel.php
+++ app/Models/ItemModel.php
@@ -71,7 +71,8 @@ class ItemModel extends BaseModel
         'package_weight',
         'competition_url',
         'products_history_values',
-        'new_status'
+        'new_status',
+        'html_mod'
     ];
 
     public function product()
@@ -1149,6 +1150,8 @@ class ItemModel extends BaseModel
                 $old_data['purchase_price'] = $data->products_value;
                 $old_data['weight'] = $data->products_weight;
                 $old_data['package_weight'] = $data->weightWithPacket;
+                //公共描述
+                $old_data['html_mod'] = $data->products_html_mod;
                 //供应商
                 $supp_name = DB::select('select suppliers_id,suppliers_company
                                         from erp_suppliers where suppliers_id = "'.$data->products_suppliers_id.'"');
@@ -1329,6 +1332,8 @@ class ItemModel extends BaseModel
                 $skuData['package_height'] = $productData['package_height'];
                 $skuData['package_width'] = $productData['package_width'];
                 $skuData['package_length'] =$productData['package_length'];
+                //
+                $skuData['html_mod'] =$data->products_html_mod;
                 //采购历史
                 $skuData['sku_history_values'] = $data->products_history_values;  
                 $skuData['status'] =$data->products_status_2;

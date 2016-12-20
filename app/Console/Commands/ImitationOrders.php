490bbb3b0c116b7b28957fae65f3fd501880d65a
mcss <178069409@qq.com>
2016-12-20 14:44:15 +0800

    no message

--- app/Console/Commands/ImitationOrders.php
+++ app/Console/Commands/ImitationOrders.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use factory;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
@@ -40,7 +41,7 @@ class ImitationOrders extends Command
     public function handle()
     {
         $quantity = $this->argument('quantity');
-        $user = factory(\App\Models\OrderModel::class,$quantity)->create(['status' => 'PREPARED','customer_service' => '63', 'operator' => '195', 'payment' => 'MIXEDCARD', 'currency' => 'USD', 'rate' => '1'])
+        $orders = factory(\App\Models\OrderModel::class,(int)$quantity)->create(['status' => 'PREPARED','customer_service' => '63', 'operator' => '195', 'payment' => 'MIXEDCARD', 'currency' => 'USD', 'rate' => '1'])
         ->each(function($single){
             $i = 0;
             $range = mt_rand(1,3);
@@ -49,5 +50,6 @@ class ImitationOrders extends Command
                 $i++;
             }
         });
+        $this->info('imitation orders data is success');
     }
 }

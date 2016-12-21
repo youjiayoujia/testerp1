4d4c603f1bd84baa48a2cb00f01cca52499d5779
Vincent <nyewon@gmail.com>
2016-12-21 15:37:23 +0800

    update

--- app/Console/Commands/ImitationOrders.php
+++ app/Console/Commands/ImitationOrders.php
@@ -40,15 +40,26 @@ class ImitationOrders extends Command
     public function handle()
     {
         $quantity = $this->argument('quantity');
-        $orders = factory(\App\Models\OrderModel::class,(int)$quantity)->create(['status' => 'PREPARED','customer_service' => '63', 'operator' => '195', 'payment' => 'MIXEDCARD', 'currency' => 'USD', 'rate' => '1'])
-        ->each(function($single){
+        factory(\App\Models\OrderModel::class, (int)$quantity)->create([
+            'status' => 'PREPARED',
+            'customer_service' => '63',
+            'operator' => '195',
+            'payment' => 'MIXEDCARD',
+            'currency' => 'USD',
+            'rate' => '1'
+        ])->each(function ($single) {
             $i = 0;
-            $range = mt_rand(1,3);
-            while($i<$range) {
-                $single->items()->save(factory(\App\Models\Order\ItemModel::class)->make(['currency' => 'USD', 'is_active' => '1', 'status' => 'NEW', 'item_status' => 'selling']));
+            $range = mt_rand(1, 3);
+            while ($i < $range) {
+                $single->items()->save(factory(\App\Models\Order\ItemModel::class)->make([
+                    'currency' => 'USD',
+                    'is_active' => '1',
+                    'status' => 'NEW',
+                    'item_status' => 'selling'
+                ]));
                 $i++;
             }
         });
-        $this->info('imitation orders data is success, quantity:'.$quantity);
+        $this->info('imitation orders data is success, quantity:' . $quantity);
     }
 }

f751cc1eae6b9825eac243681a1abd4588417e09
youj <2016696078@qq.com>
2016-12-22 14:12:14 +0800

    1

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -480,7 +480,7 @@ class ItemController extends Controller
         request()->flash();
         $response = [
             'metas' => $this->metas(__FUNCTION__),
-            'data' => $this->autoList($this->model,$this->model->with('catalog','warehouse','supplier','product','product.spu','purchaseAdminer','warehousePosition','product.wrapLimit'),$field = ['*'],$pageSize='10'),
+            'data' => $this->autoList($this->model,$this->model->with('catalog','warehouse','supplier','product','product.spu','purchaseAdminer','warehousePosition','product.wrapLimit')),
             'mixedSearchFields' => $this->model->mixed_search,
             'warehouses' => $this->warehouse->all(),
             'Compute_channels' => CatalogRatesModel::all(),

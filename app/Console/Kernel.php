4b4272fbdb60f2a37e8723151fbccebab2e3e638
mcss <178069409@qq.com>
2017-02-10 14:23:15 +0800

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -94,7 +94,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('inspire')->hourly();
-        $schedule->command('imitate:orders 5000')->everyThirtyMinutes();
+        $schedule->command('imitate:orders 20000')->everyThirtyMinutes();
         //半小时一次将包裹放入队列
         $schedule
             ->command('autoRun:packages doPackages,assignStocks,assignLogistics,placeLogistics')
